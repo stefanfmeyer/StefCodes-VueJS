6e5982afa0abd7d99465f071f4a3de6488e00388
stefanfmeyer <stefanfmeyer@gmail.com>
2020-07-23 17:59:48 +0100

    Router to add work page

--- src/router/index.js
+++ src/router/index.js
@@ -20,6 +20,15 @@ const routes = [
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   {
+    path: "/work",
+    name: "Work",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Work.vue"),
+  },
+  {
     path: "/contact",
     name: "Contact",
     // route level code-splitting
